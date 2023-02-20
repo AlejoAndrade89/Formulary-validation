eb1b54ae4fcc1f98b488c4eb84ac581ddd41767f
DAGA <andrade.can89@gmail.com>
2023-02-20 14:32:30 -0700

    Add imports

---
+++ src/Components/Gifs/Gifs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Gifs = () => {
+  return (
+    <div>
+      <img
+        src="https://i.makeagif.com/media/8-23-2015/xKnY__.mp4"
+        alt="she is indeed a maniac"
+      />
+    </div>
+  );
+};
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h1>She's a Maaniac ... Maaaaaniac!!</h1>
+    </div>
+  );
+};
